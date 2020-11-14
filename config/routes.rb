333e137774618cb3f319c2b6dddbb36061d1169f
minoru mikoda <minoru.mikoda@gmail.com>
2020-11-14 07:12:57 +0000

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :sessions
+  resources :users
   resources :feeds
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
